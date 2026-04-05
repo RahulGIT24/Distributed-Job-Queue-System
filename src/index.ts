8001f8f3b85bb94ebff4d04b90f6c842febfc2f5
rahul <rg4005450@gmail.com>
2026-04-05 16:45:39 +0530

    Basic Queue Scheduling

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+type Message = {
+    id: number,
+    task: string
+}
+
+
+const messages: Message[] = []
+const delay = (ms: number) => new Promise(res => setTimeout(res, ms));
+
+const producer = async () => {
+    let counter: number = 0;
+    while (true) {
+        counter++;
+        const newTask: Message = {
+            id: counter,
+            task: "Process Data Batch of Counter No " + counter
+        }
+        messages.push(newTask);
+        console.log("[PRODUCER] Enqueued task " + newTask.id);
+        await delay(3000);
+    }
+}
+
+const consumer = async () => {
+    console.log("[CONSUMER] Started. Waiting for Jobs to Process");
+    while (true) {
+        if(messages.length>0){
+            const currentTask = messages.shift();
+            if(currentTask){
+                console.log(`[Consumer] Working on: ${currentTask.id}`);
+                await delay(3500);
+                console.log(`[Consumer] Finished: ${currentTask.id}`);
+            }
+        }else{
+            await delay(100);
+        }
+    }
+}
+
+producer();
+consumer();
